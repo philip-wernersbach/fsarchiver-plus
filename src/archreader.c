236e67e79b41c327cbf7e4832452efc1d4a503b7
fdupoux <fdupoux@users.sourceforge.net>
2010-05-02 10:19:51 +0100

    fixed restoration of very small archives

--- src/archreader.c
+++ src/archreader.c
@@ -62,7 +62,7 @@ int archreader_destroy(carchreader *ai)
 int archreader_open(carchreader *ai)
 {   
     struct stat64 st;
-    char volhead[96];
+    char volhead[64];
     int magiclen;
     int res;
     
